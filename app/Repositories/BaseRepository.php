8cf16beab7cd64009acc4ada605a2dbe8c7c0004
Sepehryaseri <sepehryaseri95@gmail.com>
2024-06-20 17:53:12 +0330

    add base repository to project

---
+++ app/Repositories/BaseRepository.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+class BaseRepository implements BaseRepositoryInterface
+{
+
+    public function __construct(protected Model $model)
+    {
+    }
+
+    public function create(array $data): Model|Builder
+    {
+        return $this->model
+            ->query()
+            ->create($data);
+    }
+
+    public function get(array $columns): Collection|LengthAwarePaginator|array
+    {
+        $result = $this->model->query();
+        if (empty($data['page'])) {
+            return $result->get($columns);
+        }
+
+        return $result->paginate(perPage: $data['size'], columns: $columns, page: $data['page']);
+    }
+
+    public function first(int $id): Model|Builder|null
+    {
+        return $this->model->query()
+            ->where('id', $id)
+            ->first();
+    }
+
+    public function update(int $id, array $data): int
+    {
+        return $this->model->query()
+            ->where('id', $id)
+            ->update($data);
+    }
+
+    public function delete(int $id)
+    {
+        $this->model
+            ->query()
+            ->where('id', $id)
+            ->delete();
+    }
+}
+
